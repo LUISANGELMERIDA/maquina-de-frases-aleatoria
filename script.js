94bf4cfd682611b7ce70c83aa784040f5d4cc424
LUISANGELMERIDA <115110361+LUISANGELMERIDA@users.noreply.github.com>
2024-10-13 10:58:17 -0600

    se actualizo el enlace de twuitter

--- script.js
+++ script.js
@@ -51,13 +51,13 @@ function getQuote() {
 
   $('#tweet-quote').attr(
     'href',
-    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+    'https://twitter.com/' +
       encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
   );
 
   $('#tumblr-quote').attr(
     'href',
-    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+    'https://www.facebook.com/' +
       encodeURIComponent(currentAuthor) +
       '&content=' +
       encodeURIComponent(currentQuote) +
